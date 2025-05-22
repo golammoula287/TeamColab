ac441a16e3d9a5e93ec37ecd9f7cad353937a90a
golammoula287 <golammoula287@gmail.com>
2025-05-23 03:23:19 +0600

    Access3

--- server/index.js
+++ server/index.js
@@ -98,11 +98,11 @@ const PORT = process.env.PORT || 5000
 
 const app = express()
 
-// ⚠️ Allow all origins – for testing only!
 app.use(
   cors({
-    origin: "*",
+    origin: ["https://team-colab-frontend-three.vercel.app" , "http://localhost:3000"],
     methods: ["GET", "POST", "DELETE", "PUT"],
+    credentials: true,
   })
 )
 
